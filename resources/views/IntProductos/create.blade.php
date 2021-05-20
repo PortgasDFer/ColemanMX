2700027b51656e566726553d3ee21a2bfade0af4
PortgasDFer <fernando_ls96@hotmail.com>
2021-05-19 21:22:06 -0500

    CRUD Productos V1

--- resources/views/IntProductos/create.blade.php
+++ resources/views/IntProductos/create.blade.php
@@ -3,19 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <div class="card-header">{{ __('Productos') }}</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
-                    <a href="/regProductos">Productos.</a>
+                    <a href="/home">Volver al inicio.</a>
+                    <hr>
+                    
                 </div>
             </div>
         </div>
