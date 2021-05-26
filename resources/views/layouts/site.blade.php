e7cf17b40f768b26e1b66db2c940e0171222bb87
PortgasDFer <fernando_ls96@hotmail.com>
2021-05-25 22:13:12 -0500

    Slider de imagenes header

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -11,32 +11,30 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
-	<header class="header">
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-          <img src="#" alt="coleman logo" class="logo_nav">
-          <a class="navbar-brand" href="#" style="color:white;">COLEMAN MX</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto ml-auto">
-              
-            </ul>
-            <div class="navbar-nav my-2 my-lg-0">
-                <li class="nav-item">
-					<a class="nav__link" href="#">Inicio</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav__link" href="#">Catalogo</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav__link" href="#">Contacto</a>
-				</li>
-            </div>
-        </nav>
-        @yield('header')
-    </header>
+	<header>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+      <img src="{{asset('img/coleman-logo.webp')}}" alt="coleman logo" >
+      <a class="navbar-brand" href="#" style="color:white;"></a>
+      <button class="navbar-toggler nav__link" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto ml-auto">
+        </ul>
+        <div class="navbar-nav my-2 my-lg-0">
+            <li class="nav-item">
+        			<a class="nav__link" href="#">Inicio</a>
+        		</li>
+        		<li class="nav-item">
+        			<a class="nav__link" href="#">Catalogo</a>
+        		</li>
+        		<li class="nav-item">
+        			<a class="nav__link" href="#">Contacto</a>
+        		</li>
+        </div>
+    </nav>
+    @yield('header')
+  </header>
     	@yield('content')
     <div class="btn-whatsapp">
 		<a href="https://api.whatsapp.com/send?phone=" target="_blank">
